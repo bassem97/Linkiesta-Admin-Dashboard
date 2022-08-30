b53e09efa6f753c9d5bcf4288f18beccd817d8e8
BASSEM <bassem.jadoui@esprit.tn>
2022-08-30 13:37:03 +0100

    logout done ✅✅

--- src/layouts/dashboard/AccountPopover.js
+++ src/layouts/dashboard/AccountPopover.js
@@ -1,118 +1,124 @@
 import {useEffect, useRef, useState} from 'react';
-import { Link as RouterLink } from 'react-router-dom';
+import {Link as RouterLink, useNavigate} from 'react-router-dom';
 // @mui
-import { alpha } from '@mui/material/styles';
-import { Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton } from '@mui/material';
+import {alpha} from '@mui/material/styles';
+import {Box, Divider, Typography, Stack, MenuItem, Avatar, IconButton} from '@mui/material';
 // components
 import MenuPopover from '../../components/MenuPopover';
 import {AUTH_USER} from "../../utils/Redux/reducers/AuthorizationReducers";
+import {logout} from "../../utils/Redux/actions/AuthAction";
+import {useAuthDispatch} from "../../utils/Context";
 // mocks_
 
 // ----------------------------------------------------------------------
 
 const MENU_OPTIONS = [
-  {
-    label: 'Home',
-    icon: 'eva:home-fill',
-    linkTo: '/',
-  },
-  {
-    label: 'Profile',
-    icon: 'eva:person-fill',
-    linkTo: '#',
-  },
-  {
-    label: 'Settings',
-    icon: 'eva:settings-2-fill',
-    linkTo: '#',
-  },
+    {
+        label: 'Home',
+        icon: 'eva:home-fill',
+        linkTo: '/',
+    },
+    {
+        label: 'Profile',
+        icon: 'eva:person-fill',
+        linkTo: '#',
+    },
+    {
+        label: 'Settings',
+        icon: 'eva:settings-2-fill',
+        linkTo: '#',
+    },
 ];
 
 // ----------------------------------------------------------------------
 
 export default function AccountPopover() {
     const [user, setUser] = useState()
-  const anchorRef = useRef(null);
-  const [open, setOpen] = useState(null);
+    const anchorRef = useRef(null);
+    const [open, setOpen] = useState(null);
+    const dispatch = useAuthDispatch()
+    const navigate = useNavigate();
 
-  const handleOpen = (event) => {
-    setOpen(event.currentTarget);
-  };
 
-  const handleClose = () => {
-    setOpen(null);
-  };
+    const handleOpen = (event) => {
+        setOpen(event.currentTarget);
+    };
 
-    useEffect( () => {
-        AUTH_USER.then( res => {
+    const handleClose = () => {
+        logout(dispatch)
+        navigate('/')
+        setOpen(null);
+    };
+
+    useEffect(() => {
+        AUTH_USER.then(res => {
             setUser(res.data)
         })
     }, []);
 
 
+    return (
+        <>
+            <IconButton
+                ref={anchorRef}
+                onClick={handleOpen}
+                sx={{
+                    p: 0,
+                    ...(open && {
+                        '&:before': {
+                            zIndex: 1,
+                            content: "''",
+                            width: '100%',
+                            height: '100%',
+                            borderRadius: '50%',
+                            position: 'absolute',
+                            bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8),
+                        },
+                    }),
+                }}
+            >
+                <Avatar src={user?.photoURL || '/static/mock-images/avatars/avatar_default.jpg'} alt="photoURL"/>
+            </IconButton>
 
-  return (
-    <>
-      <IconButton
-        ref={anchorRef}
-        onClick={handleOpen}
-        sx={{
-          p: 0,
-          ...(open && {
-            '&:before': {
-              zIndex: 1,
-              content: "''",
-              width: '100%',
-              height: '100%',
-              borderRadius: '50%',
-              position: 'absolute',
-              bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8),
-            },
-          }),
-        }}
-      >
-        <Avatar src={user?.photoURL ||  '/static/mock-images/avatars/avatar_default.jpg'} alt="photoURL" />
-      </IconButton>
+            <MenuPopover
+                open={Boolean(open)}
+                anchorEl={open}
+                onClose={handleClose}
+                sx={{
+                    p: 0,
+                    mt: 1.5,
+                    ml: 0.75,
+                    '& .MuiMenuItem-root': {
+                        typography: 'body2',
+                        borderRadius: 0.75,
+                    },
+                }}
+            >
+                <Box sx={{my: 1.5, px: 2.5}}>
+                    <Typography variant="subtitle2" noWrap>
+                        {user?.firstName} {user?.lastName}
+                    </Typography>
+                    <Typography variant="body2" sx={{color: 'text.secondary'}} noWrap>
+                        {user?.email}
+                    </Typography>
+                </Box>
 
-      <MenuPopover
-        open={Boolean(open)}
-        anchorEl={open}
-        onClose={handleClose}
-        sx={{
-          p: 0,
-          mt: 1.5,
-          ml: 0.75,
-          '& .MuiMenuItem-root': {
-            typography: 'body2',
-            borderRadius: 0.75,
-          },
-        }}
-      >
-        <Box sx={{ my: 1.5, px: 2.5 }}>
-          <Typography variant="subtitle2" noWrap>
-            {user?.firstName} {user?.lastName}
-          </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
-            {user?.email}
-          </Typography>
-        </Box>
+                <Divider sx={{borderStyle: 'dashed'}}/>
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+                <Stack sx={{p: 1}}>
+                    {MENU_OPTIONS.map((option) => (
+                        <MenuItem key={option.label} to={option.linkTo} component={RouterLink} onClick={handleClose}>
+                            {option.label}
+                        </MenuItem>
+                    ))}
+                </Stack>
 
-        <Stack sx={{ p: 1 }}>
-          {MENU_OPTIONS.map((option) => (
-            <MenuItem key={option.label} to={option.linkTo} component={RouterLink} onClick={handleClose}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </Stack>
+                <Divider sx={{borderStyle: 'dashed'}}/>
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
-
-        <MenuItem onClick={handleClose} sx={{ m: 1 }}>
-          Logout
-        </MenuItem>
-      </MenuPopover>
-    </>
-  );
+                <MenuItem onClick={handleClose} sx={{m: 1}}>
+                    Logout
+                </MenuItem>
+            </MenuPopover>
+        </>
+    );
 }
